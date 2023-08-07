4d1f477761244b4376ec0b4b1f24f760ddd909e7
voskpaul <paulriemii@gmail.com>
2023-08-07 14:55:55 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,12 +9,12 @@
  * @next: represents the next node
  *
  * Description: the structure of the node
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
