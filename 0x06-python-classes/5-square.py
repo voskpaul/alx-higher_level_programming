f049f70367517aba43f68c7be169d010c3bee573
voskpaul <paulriemii@gmail.com>
2023-08-29 21:12:33 +0000

    updated

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
 
 class Square:
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new square.
+    def __init__(self, size=0):
+        """Initialize a Square instance.
 
         Args:
-            size (int): The size of the new square.
+            size (int): The size of the square.
         """
         self.size = size
-@property
+
+    @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """Get the size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size to set.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -26,13 +32,31 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
 
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
+        """Print the square using '#' characters."""
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 10
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 0
+    my_square.my_print()
